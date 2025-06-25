b6553fad1d29e71045752bea2c87311268ac71e6
Martin Foldyna <martin.foldyna@me.com>
2025-06-25 19:35:11 +0200

    Remove ID parameter

--- express-backend/src/routes/currencies.js
+++ express-backend/src/routes/currencies.js
@@ -3,12 +3,6 @@ const currencyController = require('../controllers/currencyController');
 
 const router = express.Router();
 
-// All routes require id parameter (UUID) except create
-router.param('id', (req, res, next, id) => {
-  req.params.id = id;
-  next();
-});
-
 // Currency routes
 router.post('/', currencyController.create);
 router.get('/:isoCode', currencyController.get);
